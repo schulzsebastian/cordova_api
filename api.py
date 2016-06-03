6ce7f2743ea52f337e2b28fa9097387e3095ea40
schulzsebastian <schulz.siwy@gmail.com>
2016-06-03 23:53:22 +0200

    addpoints post

--- api.py
+++ api.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from flask.ext.cors import CORS
 from models import *
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/foosball', methods=['GET','POST'])
-def foosball():
+@app.route('/getplayers', methods=['GET'])
+def getplayers():
     if request.method == 'GET':
-        return jsonify({'status':'ok'})
+        result = {'data':[]}
+        rs = Player.select().dicts()
+        for row in rs:
+            result['data'].append(row)
+        return jsonify(result)
+
+@app.route('/addpoints', methods=['POST'])
+def addpoints():
+    if request.method == 'POST':
+        payload = request.get_json(force=True)
+        pid = payload['pid']
+        points = payload['points']
+        try:
+            rs = Player.select().where(Player.id == pid).get()
+            val = int(points)
+        except:
+            abort(404)
+        rs.points += val
+        rs.save()
+        return jsonify({'update':'pid:%d, points:%d' % (pid, points)})
+
 
 if __name__ == '__main__':
     app.run()
