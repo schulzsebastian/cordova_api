f383bcc034c9bb994b2a8d2b9064b710cfbbb4dd
schulzsebastian <schulz.siwy@gmail.com>
2016-06-03 21:30:05 +0200

    simple get response

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask.ext.cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/foosball', methods=['GET','POST'])
+def foosball():
+	if request.method == 'GET':
+		return jsonify({'status':'ok'})
+
+if __name__ == '__main__':
+    app.run()
